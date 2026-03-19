d50e503388ad6799d99cb5f1d99d80ae5b77b844
Ercan-Ozturk <ercanozturk788@gmail.com>
2026-03-19 22:41:37 +0300

    Project init

---
+++ ruya_website/src/App.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <section id="spacer"></section>
+        <div id="social">
+
+          <h2>Rüya Serin</h2>
+          <p>Sosyal Medya</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                YouTube
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Instagram
+              </a>
+            </li>
+              <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                TikTok
+              </a>
+            </li>
+            <li>
+                <h1 className="text-3xl font-bold underline">
+                  Hello world!
+                </h1>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+          </ul>
+        </div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
