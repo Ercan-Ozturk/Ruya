a24d1f81e3bff8ac7144423e4cdeacb2e4a5612b
Ercan-Ozturk <ercanozturk788@gmail.com>
2026-03-23 13:50:49 +0300

    Item alignment

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <>
     
         <div className='flex flex-col min-h-screen bg-[url(/bg.jpg)]  bg-auto bg-center'>
-          <div className="mx-auto content-center">
+          <div className="m-auto flex flex-col items-center">
             <div className='flex flex-col items-center justify-center m-4'>
               <img
                 className="rounded-full shadow-2xl size-40 lg:size-60"
@@ -20,16 +20,16 @@ function App() {
             <p className="text-md text-center text-white font-[Schoolbell] ">Dijital İçerik Üretici & Bağımsız Sanatçı</p>
             </div>
 
-            <div className='font-[Barriecito] flex flex-col gap-2' >
+            <div className='font-[Barriecito] flex flex-col gap-2 ' >
               <a href='https://www.youtube.com/@ruyaseriin' target='_blank' rel='noopener noreferrer'>
-                <button className="cursor-pointer flex flex-row-2 bg-[#FFF5D4] hover:bg-gray-400 text-[#4F4747] text-4xl  py-2 px-8 rounded-2xl shadow-xl w-92">
+                <button className="cursor-pointer flex flex-row-2 items-center bg-[#FFF5D4] hover:bg-gray-400 text-[#4F4747] text-4xl  py-2 px-8 rounded-2xl shadow-xl w-92">
                   <SocialIcon url="https://www.youtube.com/@ruyaseriin" className='grow-0'/>     
                   <span className='grow'>YouTube</span>
                   <SocialIcon url="https://www.youtube.com/@ruyaseriin" className='grow-0 opacity-0'/>
                 </button>
               </a>
               <a href='https://www.instagram.com/ruyaseriin/' target='_blank' rel='noopener noreferrer' >
-                <button className="cursor-pointer flex flex-row-2 bg-[#FFF5D4] hover:bg-gray-400 text-[#4F4747] text-4xl  py-2 px-8 rounded-2xl shadow-xl w-92">
+                <button className="cursor-pointer flex flex-row-2 items-center bg-[#FFF5D4] hover:bg-gray-400 text-[#4F4747] text-4xl  py-2 px-8 rounded-2xl shadow-xl w-92">
                   <SocialIcon url="https://www.instagram.com/ruyaseriin/" className='grow-0 '/>
                   <span className='grow'> 
                     Instagram
@@ -41,7 +41,7 @@ function App() {
                 </button>
               </a>
               <a href='https://www.tiktok.com/@ruyaseriin'  target='_blank' rel='noopener noreferrer'>
-                <button className="cursor-pointer flex flex-row-2 bg-[#FFF5D4] hover:bg-gray-400 text-[#4F4747] text-4xl  py-2 px-8 rounded-2xl shadow-xl w-92  ">
+                <button className="cursor-pointer flex flex-row-2 items-center bg-[#FFF5D4] hover:bg-gray-400 text-[#4F4747] text-4xl  py-2 px-8 rounded-2xl shadow-xl w-92  ">
                   
                   <SocialIcon url="https://www.tiktok.com/@ruyaseriin" className='grow-0'/>
                   <span className='grow'>TikTok</span>
@@ -50,7 +50,7 @@ function App() {
                 </button>
               </a>
               <a href='https://www.x.com/ruyaseriin' target='_blank' rel='noopener noreferrer'>
-                <button className="cursor-pointer flex flex-row-2 bg-[#FFF5D4] hover:bg-gray-400 text-[#4F4747] text-4xl  py-2 px-8 rounded-2xl shadow-xl w-92">
+                <button className="cursor-pointer flex flex-row-2 items-center bg-[#FFF5D4] hover:bg-gray-400 text-[#4F4747] text-4xl  py-2 px-8 rounded-2xl shadow-xl w-92">
                   <SocialIcon url="https://www.x.com/ruyaseriin" className='grow-0' />
                   <span className='grow'>Twitter(X)</span>
                   <SocialIcon url="https://www.x.com/ruyaseriin" className="opacity-0" />
